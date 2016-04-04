9a1e5ac292f86e84639798b4579bd6c894ec11e3
Nomi Vos <nvoswork@gmail.com>
2016-04-04 09:31:25 -0500

    Added files via upload

--- guide.rb
+++ guide.rb
@@ -1,20 +1,87 @@
+require 'restaurant'
+require 'support/string_extend'
 class Guide
+
+  class Config
+    @@actions = ['list', 'find', 'add', 'quit']
+    def self.actions; @@actions; end
+  end
 
   def initialize(path = nil)
     # locate the restaurant text file at path
-    # or create a new file
-    # exit if create fails
-
+    Restaurant.filepath = path
+    if Restaurant.file_usable?
+      puts "Found restaurant file."
+      # or create a new file
+    elsif Restaurant.create_file
+      puts "Created restaurant file."
+      # exit if create fails
+    else
+      puts "Exiting. \n\n"
+      exit!
+    end
   end
 
   def launch!
     introduction
     # action loop
-    #     what do you want to do? (list, find, add, quit)
-    #     do that action
-    # repeat until user quits
+    result = nil
+    until result == :quit
+      action = get_action
+      result = do_action(action)
+    end
     conclusion
   end
+
+  def get_action
+    action = nil
+    until Guide::Config.actions.include?(action)
+      puts "Actions: " + Guide::Config.actions.join(", ") if action
+      print "> "
+      user_response = gets.chomp
+      args = user_response.downcase.strip.split(' ')
+    end
+    return action
+  end
+
+  def do_action(action)
+    case action
+    when 'list'
+      list
+    when 'find'
+      find
+    when 'add'
+      add
+    when 'quit'
+      return :quit
+    else
+      puts "\nI don't understand that command. \n"
+    end
+  end
+
+  def list
+    output_action_header("Listing restaurants")
+    restaurants = Restaurant.saved_restaurants
+    output_restaurant_table(restaurants)
+  end
+
+  def find(keyword="")
+    output_action_header("Find a restaurant")
+  end
+
+
+  def add
+    output_action_header("Add a restaurant")
+    restaurant = Restaurant.build_using_questions
+
+    if restaurant.save
+      puts "\nRestaurant Added\n\n"
+    else
+      puts "\nSave Error:  Restaurant not added\n\n"
+    end
+
+  end
+
 
   def introduction
     puts "\n\n<<< Welcome to the Food Finder >>>\n\n"
@@ -23,6 +90,27 @@ class Guide
 
   def conclusion
     puts "\n<<< Goodbye and bon Appetit! >>>\n\n\n"
+  end
+
+  private
+
+  def output_action_header(text)
+    puts "\n#{text.upcase.center(60)}\n\n"
+  end
+
+  def output_restaurant_table(restaurants=[])
+    print " " + "Name".ljust(30)
+    print " " + "Cuisine".ljust(20)
+    print " " + "Price".rjust(6) + "\n"
+    puts  "-" * 60
+    restaurants.each do |rest|
+      line =  " " << rest.name.titleize.ljust(30)
+      line << " " + rest.cuisine.titleize.ljust(20)
+      line << " " + rest.formatted_price.rjust(6)
+      puts line
+    end
+    puts "No listing found" if restaurants.empty?
+    puts "-" * 60
   end
 
 end
